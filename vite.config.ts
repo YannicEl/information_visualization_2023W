c0052c357677aa160d48f97e4e7bd6a629fbef9a
Yannic <me@yannic.at>
2023-12-13 21:10:18 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import Vue from '@vitejs/plugin-vue';
+import UnoCSS from 'unocss/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		Vue(),
+		UnoCSS(),
+		Components({ dts: 'src/components.d.ts' }),
+		AutoImport({
+			imports: ['vue'],
+			dts: 'src/auto-imports.d.ts',
+			vueTemplate: true,
+		}),
+	],
+});
